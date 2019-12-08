b235fd4b9026b478105ec3fbf8359e4398fb44d5
David Bielik <me@dvdblk.com>
2019-12-08 14:51:28 +0100

    Update ex04 with working example

--- ex04/ex04_ner.py
+++ ex04/ex04_ner.py
@@ -13,6 +13,7 @@ from tqdm import tqdm
 
 class Vocabulary:
     UNK_TOKEN="UNKNOWN"
+    PAD_TOKEN="PADDING"
 
     def __init__(self, add_unk=True):
         self._token_to_ids = {}
@@ -22,6 +23,14 @@ class Vocabulary:
             self.unk_index = self.add_token(Vocabulary.UNK_TOKEN)
         else:
             self.unk_index = -1
+
+    @classmethod
+    def init_from_dict(cls, idx_to_token):
+        vocab = Vocabulary(add_unk=False)
+        vocab._ids_to_token = idx_to_token
+        vocab._token_to_ids = { v:k for k,v in idx_to_token.items() }
+        vocab.unk_index = vocab.add_token(Vocabulary.UNK_TOKEN)
+        return vocab
 
     def add_token(self, token):
         if token in self._token_to_ids:
@@ -36,9 +45,7 @@ class Vocabulary:
         return self._token_to_ids.get(token, self.unk_index)
 
     def lookup_index(self, index):
-        if index not in self._ids_to_token:
-            raise KeyError("the index (%d) is not in the Vocabulary" % index)
-        return self._ids_to_token[index]
+        return self._ids_to_token.get(index, Vocabulary.UNK_TOKEN)
 
     def __len__(self):
         return len(self._token_to_ids)
@@ -52,11 +59,11 @@ class Vectorizer:
 
     def vectorize(self, sentence):
         sentences_ids = [self.token_vocab.lookup_token(t) for t in sentence]
-        return torch.tensor(sentences_ids, dtype=torch.long)
+        return np.array(sentences_ids, dtype=np.int64)
 
     def vectorize_labels(self, tags):
         tags_ids = [self.label_vocab.lookup_token(t) for t in tags]
-        return torch.tensor(tags_ids, dtype=torch.long)
+        return np.array(tags_ids, dtype=np.int64)
 
 class NERDataset(Dataset):
     
@@ -148,7 +155,8 @@ class NERDataset(Dataset):
         ensure each tensor is on the write device location.
         """
         dataloader = DataLoader(dataset=self, batch_size=batch_size,
-                                shuffle=shuffle, drop_last=drop_last)
+                                shuffle=shuffle, drop_last=drop_last,
+                                collate_fn=self._collate_fn_padd)
 
         for data_dict in dataloader:
             out_data_dict = {}
@@ -160,44 +168,105 @@ class NERDataset(Dataset):
                 out_data_dict[NERDataset.TARGET_Y]
             )
 
+    def _collate_fn_padd(self, batch):
+        # get a np array from a list of dicts in the format
+        # { 'x': [...], 'y': [...] }, np_batch is of size `batch_size`
+        np_batch = pd.DataFrame(batch).values
+        X = np_batch[:, 0] # sentences
+        Y = np_batch[:, 1] # labels
+        batch_size = len(X)
+        # get the lengths of sentences in this batch
+        lengths = [len(sentence) for sentence in X]
+        vectorizer = self.get_vectorizer()
+        pad_token = Vocabulary.PAD_TOKEN
+        pad_sentence_idx = vectorizer.token_vocab.lookup_token(pad_token) # 1
+        pad_labels_idx = vectorizer.label_vocab.lookup_token(pad_token) # -1
+        longest_sentence = max(lengths)
+        padded_X = np.ones((batch_size, longest_sentence)) * pad_sentence_idx
+        padded_Y = np.ones((batch_size, longest_sentence)) * pad_labels_idx
+
+        for i, sent_len in enumerate(lengths):
+            sentence = X[i]
+            tags = Y[i]
+            padded_X[i, 0:sent_len] = sentence[:sent_len]
+            padded_Y[i, 0:sent_len] = tags[:sent_len]
+        return { NERDataset.INPUT_X: torch.tensor(padded_X, dtype=torch.long), 
+                 NERDataset.TARGET_Y: torch.tensor(padded_Y, dtype=torch.long) }
+
+
 class RNN(nn.Module):
-    def __init__(self, embeddings, n_tags):
+    def __init__(self, embeddings, n_tags, sentence_padding_idx):
         super(RNN, self).__init__()
+        self.n_tags = n_tags
         # maps each token to an embedding_dim vector
         vocab_size, embedding_dim = embeddings.shape
-        self.embedding = nn.Embedding.from_pretrained(embeddings)
+        self.embedding = nn.Embedding.from_pretrained(
+            embeddings.float(), 
+            padding_idx=sentence_padding_idx
+        )
         lstm_hidden_dim = 50
         self.lstm = nn.LSTM(embedding_dim, lstm_hidden_dim, batch_first=True)
         self.fc = nn.Linear(lstm_hidden_dim, n_tags)
 
 
     def forward(self, inputs):
-        #apply the embedding layer that maps each token to its embedding
-        out = self.embedding(inputs)   # dim: batch_size x batch_max_len x embedding_dim
+        batch_size, longest_sentence_len = inputs.shape
 
+        #apply the embedding layer that maps each token to its embedding
+        # dim: batch_size x batch_max_len x embedding_dim
+        out = self.embedding(inputs)   
+    
         #run the LSTM along the sentences of length batch_max_len
-        out, _ = self.lstm(out)     # dim: batch_size x batch_max_len x lstm_hidden_dim                
+        # dim: batch_size x batch_max_len x lstm_hidden_dim
+        out, _ = self.lstm(out)                     
+        out = out.contiguous()
 
         #reshape the Variable so that each row contains one token
-        out = out.view(-1, out.shape[2])  # dim: batch_size*batch_max_len x lstm_hidden_dim
+        # dim: batch_size*batch_max_len x lstm_hidden_dim
+        out = out.view(-1, out.shape[2])  
 
         #apply the fully connected layer and obtain the output for each token
-        out = self.fc(out)          # dim: batch_size*batch_max_len x num_tags
+        # dim: batch_size*batch_max_len x num_tags
+        out = self.fc(out)          
 
-        return nn.functional.log_softmax(out, dim=1)   # dim: batch_size*batch_max_len x num_tags
+        # dim: batch_size*batch_max_len x num_tags
+        out = nn.functional.log_softmax(out, dim=1)   
+
+        return out
+
+    def loss(self, y_pred, y_true):
+        #y_pred = torch.argmax(y_pred, axis=2).float()
+
+        y_true = y_true.view(-1)
+
+        mask = (y_true >= 0).float()
+
+        y_true = y_true % y_pred.shape[1]
+
+        n_tokens = int(torch.sum(mask))
+
+        return -torch.sum(y_pred[range(y_pred.shape[0]), y_true]*mask)/n_tokens
 
 
 # Create token vocab / embeddings
 def load_embedding_and_create_vocab(filepath):
     embeddings = np.empty((0, 100), dtype=np.float64)
     vocab = Vocabulary()
+    print(Util.print_time_fmt("Loading the embeddings..."))
     with open(filepath, mode="r") as f:
+        line_count = len(f.readlines())
+        progress_bar = tqdm(
+            desc='', total=line_count, leave=False, ncols=Util.NCOLS
+        )
+        f.seek(0, 0)
         for line in f:
             token, *embedding = line.rstrip().split(" ")
             embedding = [np.float64(x) for x in embedding]
             vocab.add_token(token)
             embeddings = np.append(embeddings, [embedding], axis=0)
-
+            progress_bar.update()
+    progress_bar.close()
+    print(Util.print_time_fmt("Loading finished."))
     return embeddings, vocab
 
 
@@ -217,10 +286,11 @@ class TrainingRoutine:
         # Model
         vectorizer = self.dataset.get_vectorizer()
         n_tags = len(vectorizer.label_vocab)
-        model = RNN(embeddings, n_tags)
+        padding_idx = vectorizer.token_vocab.lookup_token(Vocabulary.PAD_TOKEN)
+        tag_padding_idx = vectorizer.label_vocab.lookup_token(Vocabulary.PAD_TOKEN)
+        model = RNN(embeddings, n_tags, padding_idx)
         self.model = model.to(args.device)
-        # Loss with class weights
-        self.loss_func = nn.CrossEntropyLoss()
+        self.loss_func = model.loss
         self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
 
     def start_training_routine(self, args):
@@ -282,25 +352,41 @@ class TrainingRoutine:
             self.optimizer.step()
 
             # Progress bar updates
-            bar.set_postfix_str("Loss={1:.3f}".format(loss_t))
+            bar.set_postfix_str("Loss={0:.3f}".format(loss_t))
             bar.update()
 
     def _val_step(self):
         self.dataset.set_split('dev')
         # Create the batch generator
         batch_generator = dataset.generate_batches(device=self.device,
-                                                    batch_size=batch_size)
-        loss_func = nn.CrossEntropyLoss()
-        model.eval()
+                                                    batch_size=self.batch_size)
+        self.model.eval()
+        accuracy = 0
         total_loss = 0
         # Compute loss / accuracy over the entire set
         for batch_index, (batch_X, batch_y) in enumerate(batch_generator):
-            y_pred =  model(x=batch_X)
-            loss = loss_func(y_pred, batch_y)
+            y_pred =  self.model(batch_X)
+            loss = self.loss_func(y_pred, batch_y)
             loss_t = loss.item()
             total_loss += (loss_t - total_loss) / (batch_index + 1)
+            acc_t = self.accuracy(y_pred, batch_y)
+            accuracy += (acc_t - accuracy) / (batch_index + 1)
 
-        return total_loss
+        return accuracy, total_loss
+
+    def accuracy(self, outputs, labels):
+        # reshape labels to give a flat vector of length batch_size*seq_len
+        labels = labels.view(-1)
+
+        # since PADding tokens have label -1, we can generate a mask to exclude the loss from those terms
+        mask = (labels >= 0)
+
+        # np.argmax gives us the class predicted for each token by the model
+        outputs = torch.argmax(outputs, axis=1)
+
+        # compare outputs with labels and divide by number of tokens (excluding PADding tokens)
+        result = torch.sum(outputs == labels)/torch.sum(mask).float()
+        return result
 
     def train(self, args, nr_batches, progress_bar):
         """Do the proper training steps and update the progress bar"""
@@ -315,13 +401,9 @@ class TrainingRoutine:
             )
             val_acc, val_loss = self._val_step()
 
-            # Scheduler updates the learning rate if needed
-            self.scheduler.step()
-
             # Model filename
             strings = list(map(str, [
-                self.nr_hidden_neurons, self.nr_filters, self.kernel_length,
-                self.learning_rate, self.nr_epochs
+                self.lstm_hidden_dim, self.learning_rate, self.nr_epochs
             ]))
             filename = "_".join(strings + [args.model_state_file])
             filepath = os.path.join(args.model_state_dir, filename)
@@ -349,7 +431,7 @@ class Util:
 
     @staticmethod
     def accuracy_loss_fmt(acc, loss):
-        return "Accuracy {0:.2f}%, Loss {1:.3f}".format(acc, loss)
+        return "Accuracy {0:.2f}%, Loss {1:.3f}".format(acc*100, loss)
 
     @staticmethod
     def get_model_filename(nr_hidden,
@@ -403,7 +485,7 @@ if __name__ == "__main__":
     args = Util.get_args()
     Util.setup(args)
 
-    emb, vocab = load_embedding_and_create_vocab("data/small.vocab")
+    emb, vocab = load_embedding_and_create_vocab("data/medium.vocab")
 
     embeddings_tensor = torch.tensor(emb, dtype=torch.float64).to(args.device)
 
@@ -414,11 +496,43 @@ if __name__ == "__main__":
         vocab
     )
 
+    # Train
     training_routine = TrainingRoutine(args, dataset, embeddings_tensor,
-        lstm_hidden_dim=50,
-        nr_epochs=5, 
+        lstm_hidden_dim=100,
+        nr_epochs=15, 
         batch_size=64, 
         learning_rate=0.01
     )
     training_routine.start_training_routine(args)
+
+    # Eval
+    training_routine.dataset.set_split('test')
+    # Create the batch generator
+    batch_generator = dataset.generate_batches(device=args.device,
+                                                batch_size=training_routine.batch_size,
+                                                shuffle=False)
+    training_routine.model.eval()
+    results_file = open("results.txt", mode="w")
+    tag_vocab = training_routine.dataset.get_vectorizer().label_vocab
+    for batch_index, (batch_X, batch_y) in enumerate(batch_generator):
+        y_pred =  training_routine.model(batch_X)
+        loss = training_routine.loss_func(y_pred, batch_y)
+        y_pred = torch.argmax(y_pred, axis=1)
+        y_pred = y_pred.reshape(batch_y.shape)
+        for i, (y, y_true, token) in enumerate(zip(y_pred, batch_y, batch_X)):
+            n_row = batch_index * training_routine.batch_size + i
+            row = training_routine.dataset._target_df.iloc[n_row]
+            for n_word, (pred_tag, true_tag, word_idx) in enumerate(zip(y, y_true, token)):
+                if true_tag == -1:
+                    # ignore padding
+                    break
+                results_file.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n".format(
+                    n_word+1, 
+                    row.Tokens[n_word],
+                    tag_vocab.lookup_index(true_tag.item()),
+                    "O",
+                    tag_vocab.lookup_index(pred_tag.item()),
+                    "O"
+                ))
+    results_file.close()
 
